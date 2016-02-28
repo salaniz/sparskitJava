e2a522bfb6718b7a5884615eaf7d56ccdee554ba
Johannes <jkirschnick@gmail.com>
2016-02-28 21:55:10 +0100

    add downloader script

---
+++ data/download.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# simple script to download and untar all files
+
+mkdir files
+cd files
+grep -v '^#' ../files.txt | xargs -I % sh -c 'curl  http://www.cise.ufl.edu/research/sparse/MM/%.tar.gz | tar xvz'
