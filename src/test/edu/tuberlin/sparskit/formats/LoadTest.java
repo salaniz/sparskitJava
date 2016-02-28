d2e876521038289a00fe820d8ead0075816c9e20
Johannes <jkirschnick@gmail.com>
2016-02-28 21:59:59 +0100

    Added a test which check if the JNI libraries can be loaded

---
+++ src/test/edu/tuberlin/sparskit/formats/LoadTest.java
@@ -0,0 +1,17 @@
+package edu.tuberlin.sparskit.formats;
+
+import edu.tuberlin.sparskit.Sparskit;
+import org.junit.Test;
+
+/**
+ * Created by Johannes on 28.02.2016.
+ */
+public class LoadTest {
+
+	@Test
+	public void testLoad() throws Exception {
+
+		Sparskit sparskit = new Sparskit();
+
+	}
+}
